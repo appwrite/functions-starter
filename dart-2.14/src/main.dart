cfcc1f3de7515cb0a148df98281c992c214f40d8
wess <wcope@me.com>
2022-01-28 11:25:47 -0500

    Adds error message for env var check

--- dart-2.14/src/main.dart
+++ dart-2.14/src/main.dart
@@ -1,3 +1,4 @@
+import 'dart:io' show Platform;
 import 'package:dart_appwrite/dart_appwrite.dart';
 import 'function_types.dart';
 
@@ -13,7 +14,18 @@ Teams teams = Teams(client);
 Users users = Users(client);
 
 
+Map<String, String> envVars = Platform.environment;
+
 Future<void> start(FunctionRequest request, FunctionResponse response) async {
+
+  if(
+    envVars.containsKey('APPWRITE_API_KEY') == false
+    || envVars.containsKey('APPWRITE_API_KEY') == false
+    || envVars.containsKey('APPWRITE_API_KEY') == false
+  ) {
+    print("Please set APPWRITE_FUNCTION_ENDPOINT, APPWRITE_FUNCTION_PROJECT_ID and APPWRITE_FUNCTION_API_KEY environment variables.");
+  }
+
   client
   .setEndpoint('https://YOUR_ENDPOINT/v1')
   .setProject('YOUR_PROJECT_ID')
