a42f2f511956e1506c3b572fdf7bfd5f1fcd0446
Agon Noga <github@noga.dev>
2022-10-09 11:14:11 +0300

    fix sdk updates and add 2.18

---
+++ dart-2.18/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:dart_appwrite/dart_appwrite.dart';
+
+/*
+  'req' variable has:
+    'headers' - object with request headers
+    'payload' - request body data as a string
+    'variables' - object with function variables
+
+  'res' variable has:
+    'send(text, status: status)' - function to return text response. Status code defaults to 200
+    'json(obj, status: status)' - function to return JSON response. Status code defaults to 200
+  
+  If an error is thrown, a response with code 500 will be returned.
+*/
+
+Future<void> start(final req, final res) async {
+  final client = Client();
+
+  // Uncomment the services you need, delete the ones you don't
+  // final account = Account(client);
+  // final avatars = Avatars(client);
+  // final database = Databases(client);
+  // final functions = Functions(client);
+  // final health = Health(client);
+  // final locale = Locale(client);
+  // final storage = Storage(client);
+  // final teams = Teams(client);
+  // final users = Users(client);
+
+  if (req.variables['APPWRITE_FUNCTION_ENDPOINT'] == null ||
+      req.variables['APPWRITE_FUNCTION_API_KEY'] == null) {
+    print(
+        "Environment variables are not set. Function cannot use Appwrite SDK.");
+  } else {
+    client
+        .setEndpoint(req.variables['APPWRITE_FUNCTION_ENDPOINT'])
+        .setProject(req.variables['APPWRITE_FUNCTION_PROJECT_ID'])
+        .setKey(req.variables['APPWRITE_FUNCTION_API_KEY'])
+        .setSelfSigned(status: true);
+  }
+
+  res.json({
+    'areDevelopersAwesome': true,
+  });
+}
