b0d76238ac6df60963e6d54f570e47b7a02b1af5
loks0n <22452787+loks0n@users.noreply.github.com>
2024-03-04 10:14:47 +0000

    feat: new runtimes

---
+++ php-8.3/src/index.php
@@ -0,0 +1,37 @@
+<?php
+
+require(__DIR__ . '/../vendor/autoload.php');
+
+use Appwrite\Client;
+
+// This is your Appwrite function
+// It's executed each time we get a request
+return function ($context) {
+    // Why not try the Appwrite SDK?
+    // $client = new Client();
+    // $client
+    //     ->setEndpoint('https://cloud.appwrite.io/v1')
+    //     ->setProject(getenv('APPWRITE_FUNCTION_PROJECT_ID'))
+    //     ->setKey(getenv('APPWRITE_API_KEY'));
+
+    // You can log messages to the console
+    $context->log('Hello, Logs!');
+
+    // If something goes wrong, log an error
+    $context->error('Hello, Errors!');
+
+    // The `req` object contains the request data
+    if ($context->req->method === 'GET') {
+        // Send a response with the res object helpers
+        // `res.send()` dispatches a string back to the client
+        return $context->res->send('Hello, World!');
+    }
+
+    // `res.json()` is a handy helper for sending JSON
+    return $context->res->json([
+        'motto' => 'Build like a team of hundreds_',
+        'learn' => 'https://appwrite.io/docs',
+        'connect' => 'https://appwrite.io/discord',
+        'getInspired' => 'https://builtwith.appwrite.io',
+    ]);
+};
